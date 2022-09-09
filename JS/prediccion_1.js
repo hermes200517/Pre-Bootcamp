9a425a0630a00cdb84f9533a4dbfcfaac6d9fbdd
hermes200517 <brayanarce2222@gmail.com>
2022-09-09 12:14:08 -0300

    mensaje

---
+++ JS/prediccion_1.js
@@ -0,0 +1,32 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+//esta funcion me va a mostrar "nacin en 1980"
+
+
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+//esta funcion me va amostrar  "naci en entradaañonacimiento"
+
+
+var num1 = 10 ;
+var num2 = 20;
+
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+    //esta funsion me va amostrar "la suma de num1 y num2 "
+
+
+
+    /*
+    diagrama en T 
+    variables               valores
+    sum                     30
+    */
