1faa6b66edde5bb847375d5d61b03111d9bd38bb
hermes200517 <brayanarce2222@gmail.com>
2022-09-09 12:11:03 -0300

    subir

---
+++ JS/opcional.JS.js
@@ -0,0 +1,13 @@
+var alturaNiño = 56;
+
+//Declarammos una funcion llamada muestraSiElNiñoPuedeSubirALaMontañaRusa.
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaNiño){
+    
+//Esta es una condicional, la altura del niño es una condicion.
+   if(alturaNiño > 56){
+       console.log("¡Súbete, chico!")
+        }else{
+        console.log("Lo siento, chico. Tal vez el próximo año")
+    }
+
+}
